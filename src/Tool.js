144b7a05b131fde7ea904f8184871f8771b5aaa1
Caijialinxx <caijialinxx@foxmail.com>
2018-07-24 20:41:57 +0800

    Emoji表情框UI

--- src/Tool.js
+++ src/Tool.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import './EmojisBox.css'
+
+let iconfont_emoji = document.createElement('script')
+iconfont_emoji.src = '//at.alicdn.com/t/font_606596_4iofyd5c9w94quxr.js'
+document.head.appendChild(iconfont_emoji)
 
 export default function (props) {
-  let selfProps = { iconHref: '', inputType: '' }
+  let selfProps = { iconHref: '', inputType: '', clickEvent: null }
   if (props.type === 'color') {
     selfProps.iconHref = '#icon-textcolor'
     selfProps.inputType = 'color'
@@ -10,16 +15,31 @@ export default function (props) {
     selfProps.inputType = 'file'
   } else if (props.type === 'emoji') {
     selfProps.iconHref = '#icon-emoji'
+    selfProps.clickEvent = initEmoji
   }
   return (
-    <label>
+    <label onClick={selfProps.clickEvent}>
       <svg className="icon" aria-hidden="true">
         <use xlinkHref={selfProps.iconHref}></use>
       </svg>
       {props.type === 'emoji' ?
-        <div className="emojisBox"></div> :
+        <div id='emojisBox' className="emojisBox"></div> :
         <input type={selfProps.inputType} />
       }
     </label>
   )
+}
+function initEmoji() {
+  let box = document.getElementById('emojisBox'),
+    docFragment = document.createDocumentFragment()
+  let index
+  for (let i = 1; i <= 30; i++) {
+    if (i < 10) { index = '0' + i }
+    else { index = String(i) }
+    let iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    iconSvg.innerHTML = '<use xlink:href="#icon-emoji-' + index + '"></use>'
+    docFragment.appendChild(iconSvg)
+  }
+  box.appendChild(docFragment)
+  box.style.display = 'block'
 }
