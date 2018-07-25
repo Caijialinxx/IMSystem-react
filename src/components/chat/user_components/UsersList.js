69f93ed0d00ee854619f3ec21252ce67caa39725
Caijialinxx <caijialinxx@foxmail.com>
2018-07-25 17:06:00 +0800

    修改未读消息的css及改善UsersList的props接收

--- src/components/chat/user_components/UsersList.js
+++ src/components/chat/user_components/UsersList.js
@@ -4,7 +4,7 @@ import './UsersList.css'
 export default class UsersList extends Component {
   render() {
     return (
-      <ul className='usersList'>
+      <ul className={this.props.className}>
         <label htmlFor="Robot">
           <li id="Robot">Robot</li>
         </label>
