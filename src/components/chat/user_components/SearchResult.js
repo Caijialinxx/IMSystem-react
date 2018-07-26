9a6fcc7b1bc0b84e6e69687c21e7d3bdbbd19e29
Caijialinxx <caijialinxx@foxmail.com>
2018-07-26 18:30:49 +0800

    使用form监听添加好友的submit事件，相应地改正HTML标签及其CSS

--- src/components/chat/user_components/SearchResult.js
+++ src/components/chat/user_components/SearchResult.js
@@ -14,10 +14,10 @@ export default class SearchFriend extends Component {
           <span className="friendName"></span>
           <span className="status"></span>
         </div>
-        <div className="action">
+        <form className="action">
           <input type="text" maxLength="30" placeholder="Authentication" />
-          <button>Add</button>
-        </div>
+          <input type="submit" value="Add" />
+        </form>
       </div>
     )
   }
