6231545961286b7e1ac80391f68a14079c2b8119
Caijialinxx <caijialinxx@foxmail.com>
2018-07-27 17:54:08 +0800

    显示emojisBox

--- src/components/chat/chat_components/Tool.js
+++ src/components/chat/chat_components/Tool.js
@@ -6,40 +6,23 @@ iconfont_emoji.src = '//at.alicdn.com/t/font_606596_4iofyd5c9w94quxr.js'
 document.head.appendChild(iconfont_emoji)
 
 export default function (props) {
-  let selfProps = { iconHref: '', inputType: '', clickEvent: null }
+  let iconHref
   if (props.type === 'color') {
-    selfProps.iconHref = '#icon-textcolor'
-    selfProps.inputType = 'color'
+    iconHref = '#icon-textcolor'
   } else if (props.type === 'file') {
-    selfProps.iconHref = '#icon-doc'
-    selfProps.inputType = 'file'
+    iconHref = '#icon-doc'
   } else if (props.type === 'emoji') {
-    selfProps.iconHref = '#icon-emoji'
-    selfProps.clickEvent = initEmoji
+    iconHref = '#icon-emoji'
   }
   return (
-    <label onClick={selfProps.clickEvent}>
+    <label id={props.type} onClick={props.type === 'emoji' ? initEmoji.bind(undefined, props) : null}>
       <svg className="icon" aria-hidden="true">
-        <use xlinkHref={selfProps.iconHref}></use>
+        <use xlinkHref={iconHref}></use>
       </svg>
-      {props.type === 'emoji' ?
-        <div id='emojisBox' className="emojisBox"></div> :
-        <input type={selfProps.inputType} />
-      }
-    </label>
+      {props.type !== 'emoji' ? <input type={props.type} /> : null}
+    </label >
   )
 }
-function initEmoji() {
-  let box = document.getElementById('emojisBox'),
-    docFragment = document.createDocumentFragment()
-  let index
-  for (let i = 1; i <= 30; i++) {
-    if (i < 10) { index = '0' + i }
-    else { index = String(i) }
-    let iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-    iconSvg.innerHTML = '<use xlink:href="#icon-emoji-' + index + '"></use>'
-    docFragment.appendChild(iconSvg)
-  }
-  box.appendChild(docFragment)
-  box.style.display = 'block'
+function initEmoji(props) {
+  props.onClick()
 }
--- src/components/chat/chat_components/Tools.js
+++ src/components/chat/chat_components/Tools.js
@@ -3,13 +3,54 @@ import './Tools.css'
 import Tool from './Tool'
 
 export default class Tools extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isEmojisBoxInited: false
+    }
+  }
   render() {
     return (
-      <div className="toolsWrapper">
-        <Tool type='emoji' />
+      <div className="toolsWrapper" onClick={this.stopPropagation.bind(this)}>
+        <Tool id='emoji' type='emoji' onClick={this.initEmojis.bind(this)} />
         <Tool type='color' />
         <Tool type='file' />
       </div >
     )
+  }
+  initEmojis() {
+    let box
+    if (this.state.isEmojisBoxInited === false) {
+      box = document.createElement('div')
+      let docFragment = document.createDocumentFragment()
+      box.className = 'emojisBox'
+      let index
+      for (let i = 1; i <= 30; i++) {
+        if (i < 10) { index = '0' + i }
+        else { index = String(i) }
+        let iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+        iconSvg.innerHTML = '<use xlink:href="#icon-emoji-' + index + '"></use>'
+        docFragment.appendChild(iconSvg)
+      }
+      box.appendChild(docFragment)
+      document.querySelector('label#emoji').appendChild(box)
+      this.state.isEmojisBoxInited = true
+    } else {
+      box = document.querySelector('.emojisBox')
+    }
+    console.log('btn click')
+    box.style.display = (box.style.display === '' || box.style.display === 'none') ? 'block' : 'none'
+    console.log('show')
+    setTimeout(function () {
+      document.body.addEventListener('click', function () {
+        console.log('body click')
+        box.style.display = 'none'
+        console.log('hide')
+      }, { once: true })
+    }, 0)
+  }
+  stopPropagation(e) {
+    e.stopPropagation()
+    console.log('阻止冒泡，bodyClick不执行')
   }
 }
